1a96620fbddaafa8bd79e54e18fcb254be6fc817
UDFrost <undeadfrost@live.com>
2018-12-07 18:12:31 +0800

    修改相关配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,19 @@
 /* config-overrides.js */
 const {injectBabelPlugin} = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less-modules');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = function override(config, env) {
 	//do stuff with the webpack config...
+	if (env === 'production') {
+		config.devtool = false
+		if (!config.plugins) {
+			config.plugins = [];
+		}
+		config.plugins.push(
+			new CompressionPlugin()
+		)
+	}
 	config = injectBabelPlugin(
 		['import', {libraryName: 'antd', libraryDirectory: 'es', style: 'css'}],
 		config,
