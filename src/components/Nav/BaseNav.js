36a042b23f0b66497beb61bcbcb51b90d15d2530
UDFrost <undeadfrost@live.com>
2019-01-10 13:30:29 +0800

    解决Menu收起展开Bug问题

--- src/components/Nav/BaseNav.js
+++ src/components/Nav/BaseNav.js
@@ -1,62 +1,10 @@
 import React, {Component} from 'react'
-import {withRouter} from 'react-router'
 import {Link} from 'react-router-dom'
 import {Icon, Menu} from 'antd'
 
 const SubMenu = Menu.SubMenu
 
 class BaseNav extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			openKeys: [],
-			selectedKeys: [],
-		}
-	}
-	
-	componentDidMount() {
-		this.getKeys()
-	}
-	
-	onClick = ({item, key, keyPath}) => {
-		this.setState({selectedKeys: [key]})
-	}
-	
-	// 菜单只能展开一项
-	onOpenChange = (openKeys) => {
-		if (openKeys.length <= 1) {
-			this.setState({openKeys: openKeys});
-		} else {
-			this.setState({openKeys: [openKeys[openKeys.length - 1]]})
-		}
-	}
-	
-	getKeys = () => {
-		let pathname = this.props.history.location.pathname
-		// 修改进入/admin或/admin/跳转后导航无法高亮问题
-		if (pathname === '/admin/' || pathname === '/admin') pathname = '/admin/welcome'
-		const navsData = this.props.navsData
-		let openKeys = []
-		let selectedKeys = []
-		for (let i = 0; i < navsData.length; i++) {
-			if (pathname.includes(navsData[i].route)) {
-				selectedKeys.push(navsData[i].id.toString())
-				break
-			}
-			const submenus = navsData[i].submenus
-			if (submenus) {
-				for (let j = 0; j < submenus.length; j++) {
-					if (submenus[j].route === pathname) {
-						openKeys.push(navsData[i].id.toString())
-						selectedKeys.push(submenus[j].id.toString())
-						break
-					}
-				}
-			}
-		}
-		this.setState({openKeys: openKeys, selectedKeys: selectedKeys})
-	}
-	
 	getMenuItems = (menusData) => {
 		if (!menusData) {
 			return [];
@@ -94,21 +42,17 @@ class BaseNav extends Component {
 	}
 	
 	render() {
-		const navsData = this.props.navsData
-		const navFold = this.props.navFold
-		console.log(navFold)
+		const {navsData, ...props} = this.props
 		return (
 			<Menu
 				theme="dark"
 				mode="inline"
-				openKeys={this.state.openKeys}
-				selectedKeys={this.state.selectedKeys}
-				onClick={this.onClick}
-				onOpenChange={this.onOpenChange}>
+				{...props}
+				onSelect={this.onSelect}>
 				{this.getMenuItems(navsData)}
 			</Menu>
 		)
 	}
 }
 
-export default withRouter(BaseNav)
+export default BaseNav
